046d38e054988fa39cd9739d8441cb51f1881689
Hackr <81916900+123452123-codr@users.noreply.github.com>
2021-08-22 23:10:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,16 @@
     messages.appendChild(newMessage);
     text.value = "";
   });
+
+  text.addEventListener("keyup", function(event){
+    var key = event.which || event.keycode;
+    if(key == 13)
+    {
+      event.preventDefault();
+      
+      var newMessage = document.createElement("li");
+      newMessage.innerHTML = text.value;
+      messages.appendChild(newMessage);
+      text.value = "";
+    }
+  });
